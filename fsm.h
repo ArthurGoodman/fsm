c0f69ff209163a8f61b3f1a40513f51f478af8ef
Arthur Goodman <arthur.goodman3@gmail.com>
2016-06-04 23:55:12 +0300

    min

--- fsm.h
+++ fsm.h
@@ -14,22 +14,25 @@ class Fsm;
 
 template <class S>
 class fsm::Fsm {
-    std::vector<S> q;
-    std::vector<char> a;
-    Matrix<std::set<int>> t;
-    std::set<int> s;
-    std::set<int> f;
+    std::vector<S> states;
+    std::vector<char> alphabet;
+    Matrix<std::set<int>> transitions;
+    std::set<int> startingStates;
+    std::set<int> finalStates;
 
 public:
-    Fsm(const std::vector<S> &q, const std::vector<char> &a);
-    Fsm(const std::vector<S> &q, const std::vector<char> &a, std::set<int> s, std::set<int> f);
-    Fsm(const std::vector<S> &q, const std::vector<char> &a, const std::vector<std::vector<std::set<int>>> &t);
-    Fsm(const std::vector<S> &q, const std::vector<char> &a, const std::vector<std::vector<std::set<int>>> &t, std::set<int> s, std::set<int> f);
+    Fsm(const std::vector<S> &states, const std::vector<char> &alphabet);
+    Fsm(const std::vector<S> &states, const std::vector<char> &alphabet, std::set<int> startingStates, std::set<int> finalStates);
+    Fsm(const std::vector<S> &states, const std::vector<char> &alphabet, const std::vector<std::vector<std::set<int>>> &transitions);
+    Fsm(const std::vector<S> &states, const std::vector<char> &alphabet, const std::vector<std::vector<std::set<int>>> &transitions, std::set<int> startingStates, std::set<int> finalStates);
 
-    void connect(int s1, int s2, char c);
-    void connect(int s1, int s2, int c);
-    void start(int s);
-    void finish(int s);
+    Fsm(const Fsm<S> &fsm);
+    Fsm(Fsm<S> &&fsm);
+
+    void connect(int s1, int s2, char a);
+    void connect(int s1, int s2, int a);
+    void start(int startingStates);
+    void finish(int startingStates);
 
     void inspect() const;
 
@@ -39,55 +42,66 @@ public:
 
 private:
     void printState(int state) const;
+    std::vector<std::set<int>> closure(std::vector<std::set<int>> &q, int state) const;
 };
 
 template <class S>
-fsm::Fsm<S>::Fsm(const std::vector<S> &q, const std::vector<char> &a)
-    : q(q), a(a), t(a.size(), q.size()) {
+fsm::Fsm<S>::Fsm(const std::vector<S> &states, const std::vector<char> &alphabet)
+    : states(states), alphabet(alphabet), transitions(alphabet.size(), states.size()) {
 }
 
 template <class S>
-fsm::Fsm<S>::Fsm(const std::vector<S> &q, const std::vector<char> &a, std::set<int> s, std::set<int> f)
-    : q(q), a(a), t(a.size(), q.size()), s(s), f(f) {
+fsm::Fsm<S>::Fsm(const std::vector<S> &states, const std::vector<char> &alphabet, std::set<int> startingStates, std::set<int> finalStates)
+    : states(states), alphabet(alphabet), transitions(alphabet.size(), states.size()), startingStates(startingStates), finalStates(finalStates) {
 }
 
 template <class S>
-fsm::Fsm<S>::Fsm(const std::vector<S> &q, const std::vector<char> &a, const std::vector<std::vector<std::set<int>>> &t)
-    : q(q), a(a), t(t) {
+fsm::Fsm<S>::Fsm(const std::vector<S> &states, const std::vector<char> &alphabet, const std::vector<std::vector<std::set<int>>> &transitions)
+    : states(states), alphabet(alphabet), transitions(transitions) {
 }
 
 template <class S>
-fsm::Fsm<S>::Fsm(const std::vector<S> &q, const std::vector<char> &a, const std::vector<std::vector<std::set<int>>> &t, std::set<int> s, std::set<int> f)
-    : q(q), a(a), t(t), s(s), f(f) {
+fsm::Fsm<S>::Fsm(const std::vector<S> &states, const std::vector<char> &alphabet, const std::vector<std::vector<std::set<int>>> &transitions, std::set<int> startingStates, std::set<int> finalStates)
+    : states(states), alphabet(alphabet), transitions(transitions), startingStates(startingStates), finalStates(finalStates) {
 }
 
 template <class S>
-void fsm::Fsm<S>::connect(int s1, int s2, char c) {
-    t[s1][std::find(a.begin(), a.end(), c) - a.begin()].insert(s2);
+fsm::Fsm<S>::Fsm(const Fsm<S> &fsm)
+    : states(fsm.states), alphabet(fsm.alphabet), transitions(fsm.transitions), startingStates(fsm.startingStates), finalStates(fsm.finalStates) {
 }
 
 template <class S>
-void fsm::Fsm<S>::connect(int s1, int s2, int c) {
-    t[s1][c].insert(s2);
+fsm::Fsm<S>::Fsm(Fsm<S> &&fsm)
+    : states(std::move(fsm.states)), alphabet(std::move(fsm.alphabet)), transitions(std::move(fsm.transitions)), startingStates(std::move(fsm.startingStates)), finalStates(std::move(fsm.finalStates)) {
+}
+
+template <class S>
+void fsm::Fsm<S>::connect(int s1, int s2, char a) {
+    transitions[s1][std::find(alphabet.begin(), alphabet.end(), a) - alphabet.begin()].insert(s2);
+}
+
+template <class S>
+void fsm::Fsm<S>::connect(int s1, int s2, int a) {
+    transitions[s1][a].insert(s2);
 }
 
 template <class S>
 void fsm::Fsm<S>::start(int state) {
-    s.insert(state);
+    startingStates.insert(state);
 }
 
 template <class S>
 void fsm::Fsm<S>::finish(int state) {
-    f.insert(state);
+    finalStates.insert(state);
 }
 
 template <class S>
 void fsm::Fsm<S>::inspect() const {
-    for (int s1 = 0; s1 < (int)q.size(); s1++)
-        for (int c = 0; c < (int)a.size(); c++)
-            for (int s2 : t.at(s1, c)) {
+    for (int s1 = 0; s1 < (int)states.size(); s1++)
+        for (int c = 0; c < (int)alphabet.size(); c++)
+            for (int s2 : transitions.at(s1, c)) {
                 printState(s1);
-                std::cout << " --" << a[c] << "-> ";
+                std::cout << " --" << alphabet[c] << "-> ";
                 printState(s2);
                 std::cout << "\n";
             }
@@ -95,19 +109,38 @@ void fsm::Fsm<S>::inspect() const {
 
 template <class S>
 fsm::Fsm<S> fsm::Fsm<S>::rev() const {
-    Fsm<S> rfsm(q, a, f, s);
+    Fsm<S> rfsm(states, alphabet, finalStates, startingStates);
 
-    for (int s1 = 0; s1 < (int)q.size(); s1++)
-        for (int c = 0; c < (int)a.size(); c++)
-            for (int s2 : t.at(s1, c))
-                rfsm.connect(s2, s1, (int)c);
+    for (int s1 = 0; s1 < (int)states.size(); s1++)
+        for (int a = 0; a < (int)alphabet.size(); a++)
+            for (int s2 : transitions.at(s1, a))
+                rfsm.connect(s2, s1, a);
 
     return rfsm;
 }
 
 template <class S>
 fsm::Fsm<S> fsm::Fsm<S>::det() const {
-    return *this;
+    std::vector<std::set<int>> q;
+
+    q.push_back(startingStates);
+
+    std::vector<std::vector<std::set<int>>> newTransitions;
+
+    while (newTransitions.size() < q.size())
+        newTransitions.push_back(closure(q, newTransitions.size()));
+
+    std::vector<S> newStates(q.size());
+    std::iota(newStates.begin(), newStates.end(), 0);
+
+    std::set<int> newFinalStates;
+
+    for (int i = 0; i < (int)q.size(); i++)
+        for (int s : q[i])
+            if (finalStates.find(s) != finalStates.end())
+                newFinalStates.insert(i);
+
+    return Fsm(newStates, alphabet, newTransitions, {0}, newFinalStates);
 }
 
 template <class S>
@@ -117,5 +150,37 @@ fsm::Fsm<S> fsm::Fsm<S>::min() const {
 
 template <class S>
 void fsm::Fsm<S>::printState(int state) const {
-    std::cout << (s.find(state) != s.end() ? "*" : " ") << q[state] << (f.find(state) != f.end() ? "*" : " ");
+    std::cout << (startingStates.find(state) != startingStates.end() ? "*" : " ") << states[state] << (finalStates.find(state) != finalStates.end() ? "*" : " ");
+}
+
+template <class S>
+std::vector<std::set<int>> fsm::Fsm<S>::closure(std::vector<std::set<int>> &q, int state) const {
+    std::vector<std::set<int>> t;
+
+    for (int a = 0; a < (int)alphabet.size(); a++) {
+        std::set<int> ts;
+
+        for (int i : q[state])
+            for (int s : transitions.at(i, a))
+                ts.insert(s);
+
+        if (ts.empty()) {
+            t.push_back(std::set<int>());
+            continue;
+        }
+
+        int index;
+
+        std::vector<std::set<int>>::iterator i = std::find(q.begin(), q.end(), ts);
+
+        if (i == q.end()) {
+            index = q.size();
+            q.push_back(ts);
+        } else
+            index = i - q.begin();
+
+        t.push_back(std::set<int>({index}));
+    }
+
+    return t;
 }
--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main() {
     fsm::Fsm<int> fsm({0, 1, 2, 3}, {'a', 'b'}, {{{1, 2}, {2}}, {{2}, {3}}, {{1, 2}, {3}}, {{}, {}}}, {0}, {3});
     fsm.inspect();
     std::cout << "\n";
-    fsm.rev().inspect();
+    fsm.min().inspect();
 
     return 0;
 }
